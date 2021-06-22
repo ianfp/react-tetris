4f3beedf5492561c466fe35aa341ff5ff8bedd3a
Ian Phillips <ifphillips@gmail.com>
2021-06-21 21:22:35 -0700

    Render the empty board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const height = 20;
+    const width = 10;
+    return (
+        <div className="App">
+            <Board height={height} width={width}/>
+        </div>
+    );
 }
 
 export default App;
+
+class Piece {
+    constructor(x, y, color) {
+        this.position = pos(x, y);
+        this.color = color;
+    }
+}
+
+function pos(x, y) {
+    return {x: x, y: y};
+}
+
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pieces: [
+                new Piece(3, 4),
+            ],
+        };
+    }
+
+    render() {
+        console.log('props', this.props);
+        const rows = repeat(this.props.height, (elem, index) => <Row key={index} width={this.props.width}/>);
+        return (
+            <div className="board">
+                {rows}
+            </div>
+        );
+    }
+}
+
+function Row(props) {
+    const cells = repeat(props.width, (elem, index) => <div key={index} className="cell"/> );
+    return (
+        <div className="row">{cells}</div>
+    );
+}
+
+function repeat(count, mapFn) {
+    return Array.from({length: count}, mapFn);
+}
